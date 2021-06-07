fb77c7eb91e84e5bc4ac3150136138143b87f9d8
mostafa1912 <mostafa1912@gmail.com>
2021-06-07 19:46:17 +0200

    .

--- The Conqueror/src/engine/Player.java
+++ The Conqueror/src/engine/Player.java
@@ -205,6 +205,36 @@ public class Player {
 	 }
 	
 	
+<<<<<<< Updated upstream
+=======
+	 public void upgradeBuilding(Building b) throws NotEnoughGoldException,
+	 BuildingInCoolDownException, MaxLevelException  
+
+	//Setters and Getters
+	public double getTreasury() {
+		return treasury;
+	}
+	public void setTreasury(double treasury) {
+		this.treasury = treasury;
+	}
+	public double getFood() {
+		return food;
+	}
+	public void setFood(double food) {
+		this.food = food;
+	}
+	public String getName() {
+		return name;
+	}
+	public ArrayList<City> getControlledCities() {
+		return controlledCities;
+	}
+	public ArrayList<Army> getControlledArmies() {
+		return controlledArmies;
+	}
+
+	
+>>>>>>> Stashed changes
 	
 	
 
