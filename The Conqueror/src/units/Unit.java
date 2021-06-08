0d4883bc1a4c908ac93d71376026a2c501fcc534
SeifElbayomi <seifhuaweiy52017@gmail.com>
2021-06-08 22:09:26 +0200

    Attack Method built without CSV

--- The Conqueror/src/units/Unit.java
+++ The Conqueror/src/units/Unit.java
@@ -72,7 +72,7 @@ public class Unit{
 	}
 	
 	
-	
+	/*
 	 public void attack(Unit target) throws FriendlyFireException , IOException
 	    {	
 		 
@@ -89,18 +89,26 @@ public class Unit{
 	    		type = "Cavalry";
 	    	
 	    	String path = type + "_factor.csv";    	
-	    	String currentLine = "";	
+	    	String currentLine = "";
+	    	System.out.println("****************");
 			FileReader fr = new FileReader(path);
 			BufferedReader br = new BufferedReader(fr);
-	    	if (this instanceof Archer) { 
-		
-			
+	    	
 			while ((currentLine = br.readLine()) != null ) {
+			
+				System.out.println(currentLine);
+				if (this instanceof Archer) { 
+				
+			
+				System.out.println("dddddddddddddddd");
 				String[] line = currentLine.split(",");
+				System.out.println(line);
 				if ( getLevel() == Integer.parseInt(line[0]))
 						factor = Integer.parseInt(line[2]);
+				System.out.println(line[0]);
+				System.out.println(line[2]);
 			}
-			}
+			
 	    	else if (this instanceof Infantry) { 
 	    		for(int i =0; i < 4 ; i++) 
 	    			{currentLine = br.readLine();}
@@ -125,11 +133,131 @@ public class Unit{
 	    		
 	    		
 	        	}
+			}
 	    	br.close();
 	    	
 	    	
 	    	currentSoldierCount = (int)(currentSoldierCount - (currentSoldierCount*factor));
 	    }
+			
+	 */
+	 
+	 public void attack(Unit target) throws FriendlyFireException { 
+		 
+		 if (this.getParentArmy().equals(target.getParentArmy()))
+		 		throw new FriendlyFireException("The current unit can't attack this target as they belong to the same Army ");
+		 	
+		 double factor = 0.0 ; 
+		 
+		 if (this instanceof Archer) { 
+			 
+				 if (target instanceof Archer) {
+						 if (target.getLevel() ==1)
+							 factor  =0.3;
+						 if (target.getLevel() ==2)
+							 factor  =0.4;
+						 if (target.getLevel() ==3)
+							 factor  =0.5;
+				 }
+				 
+				 if (target instanceof Infantry) {
+					 if (target.getLevel() ==1)
+						 factor  =0.2;
+					 if (target.getLevel() ==2)
+						 factor  =0.3;
+					 if (target.getLevel() ==3)
+						 factor  =0.4;
+				 }
+				 
+				 if (target instanceof Cavalry) {
+					 if (target.getLevel() ==1)
+						 factor  =0.1;
+					 if (target.getLevel() ==2)
+						 factor  =0.1;
+					 if (target.getLevel() ==3)
+						 factor  =0.2;
+				 }
+			 
+				 
+			 
+			 
+		 }
+		 
+		 
+		 if (this instanceof Infantry) { 
+			 
+			 if (target instanceof Archer) {
+					 if (target.getLevel() ==1)
+						 factor  =0.3;
+					 if (target.getLevel() ==2)
+						 factor  =0.4;
+					 if (target.getLevel() ==3)
+						 factor  =0.5;
+			 }
+			 
+			 if (target instanceof Infantry) {
+				 if (target.getLevel() ==1)
+					 factor  =0.1;
+				 if (target.getLevel() ==2)
+					 factor  =0.2;
+				 if (target.getLevel() ==3)
+					 factor  =0.3;
+			 }
+			 
+			 if (target instanceof Cavalry) {
+				 if (target.getLevel() ==1)
+					 factor  =0.1;
+				 if (target.getLevel() ==2)
+					 factor  =0.2;
+				 if (target.getLevel() ==3)
+					 factor  =0.25;
+			 }
+			 
+			 
+			 
+		 
+			 
+		 
+		 
+		 }
+		 
+		 if (this instanceof Cavalry) { 
+			 
+			 if (target instanceof Archer) {
+					 if (target.getLevel() ==1)
+						 factor  =0.5;
+					 if (target.getLevel() ==2)
+						 factor  =0.6;
+					 if (target.getLevel() ==3)
+						 factor  =0.7;
+			 }
+			 
+			 if (target instanceof Infantry) {
+				 if (target.getLevel() ==1)
+					 factor  =0.3;
+				 if (target.getLevel() ==2)
+					 factor  =0.4;
+				 if (target.getLevel() ==3)
+					 factor  =0.5;
+			 }
+			 
+			 if (target instanceof Cavalry) {
+				 if (target.getLevel() ==1)
+					 factor  =0.2;
+				 if (target.getLevel() ==2)
+					 factor  =0.2;
+				 if (target.getLevel() ==3)
+					 factor  =0.3;
+			 }
+		 
+			 
+		 
+		 
+	 }
+		 
+		 
+		 target.setCurrentSoldierCount( (int) (this.currentSoldierCount*factor));
+	 }
 	    
 	 
 	 
