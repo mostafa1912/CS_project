d1cfe62e582dbb9459c2f551249e08e6a5e40db8
mostafa1912 <mostafa1912@gmail.com>
2021-06-22 19:57:46 +0200

    creating the homescreen with background and no buttons yet

--- EmpireM2/src/GUI/Home.java
+++ EmpireM2/src/GUI/Home.java
@@ -2,8 +2,11 @@ package GUI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Home extends Application {
     Stage window;
@@ -16,8 +19,26 @@ public class Home extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        //creating layout
         BorderPane Layout1_Border_pane = new BorderPane();
-            window = new Stage();
+        //creating window stage and setting it as primary
+            window = stage;
+        //setting window title
+            window.setTitle("The Conqueror");
+        //creating the first scene and setting its layout
+            StartingView = new Scene(Layout1_Border_pane );
+        //setting stage size to full screen
+            window.setFullScreen(true);
+        //creating a background image object
+        BackgroundImage home = new BackgroundImage(new Image(new File("images/gameHomeScreen.jpg").toURI().toString()) ,
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER,
+                new BackgroundSize(1.0 ,1.0,true,true, false, false) );
+        //setting layout background
+        Layout1_Border_pane.setBackground(new Background(home));
+        //setting the scene for window
+        window.setScene(StartingView);
+        //showing stage to user
+        window.show();
 
 
     }
