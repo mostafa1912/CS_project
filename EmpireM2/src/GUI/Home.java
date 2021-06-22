fbf3b88ef3f4e3f19897cbc502be1c6669272f06
mostafa1912 <mostafa1912@gmail.com>
2021-06-22 18:51:02 +0200

    d

--- EmpireM2/src/GUI/Home.java
+++ EmpireM2/src/GUI/Home.java
@@ -1,4 +1,4 @@
-package view;
+package GUI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
--- EmpireM2/src/GUI/HomeScreen.java
+++ EmpireM2/src/GUI/HomeScreen.java
@@ -1,4 +1,4 @@
-package view;
+package GUI;
 import engine.Game;
 import engine.Player;
 import javafx.application.Application;
