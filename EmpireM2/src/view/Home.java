2c801f1cc6c2305fb042add397bc1973975f3619
mostafa1912 <mostafa1912@gmail.com>
2021-06-22 18:49:43 +0200

    d

--- EmpireM2/src/view/Home.java
+++ EmpireM2/src/view/Home.java
@@ -1,11 +1,24 @@
 package view;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Home extends Application {
+    Stage window;
+    Scene StartingView;
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
+        BorderPane Layout1_Border_pane = new BorderPane();
+            window = new Stage();
+
 
     }
 }
