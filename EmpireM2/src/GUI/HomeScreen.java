b560aba4223f2aa73e1451c6d5a1a6d67358967b
mostafa1912 <mostafa1912@gmail.com>
2021-06-23 02:06:48 +0200

    Update HomeScreen.java

--- EmpireM2/src/GUI/HomeScreen.java
+++ EmpireM2/src/GUI/HomeScreen.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import engine.*;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.NodeOrientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -38,46 +37,23 @@ public class HomeScreen extends Application {
 		Stage window = new Stage();
 		primaryWindow = window;
 		window.setTitle("The Conqueror");
-		
+
 		game = null;
 
 
-		
+
 		scene1(window);
-		
+
 		primaryWindow.show();
 	}
-	
-	
-	
+
+
+
 	public void scene1(Stage window) {
 
 		StackPane startLayout = new StackPane();// Start pane
 
-<<<<<<< HEAD
 		//creating a background image object
-=======
-		Image gamenamelogo = new Image("file:images/gamenamelogo.png");
-		ImageView gamenamelogoView = new ImageView(gamenamelogo);
-		
-		VBox gamenamelogoViewVBox = new VBox();
-		gamenamelogoViewVBox.getChildren().add(gamenamelogoView);
-		
-		gamenamelogoViewVBox.setAlignment(Pos.TOP_CENTER);
-		
-		
-		
-		Image deepquote = new Image("file:images/deepquote.png");
-		ImageView deepquoteView = new ImageView(deepquote);
-		
-		VBox deepquoteViewVBox = new VBox();
-		deepquoteViewVBox.getChildren().add(deepquoteView);
-		deepquoteViewVBox.setAlignment(Pos.BOTTOM_CENTER);
-		
-		
-// Making Image as wallpaper 
-// will add start pic and button
->>>>>>> bb2221f4b5b4df7ccfdaf9399b18502efa34c9d9
 		BackgroundImage bg = new BackgroundImage(new Image("file:images/gameHomeScreen.jpg"),
 				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
 				BackgroundPosition.CENTER, new BackgroundSize(1.0 ,1.0,true,true, false, false) );
@@ -89,43 +65,26 @@ public class HomeScreen extends Application {
 		//setting button size and font
 		startButton.setMaxHeight(50);
 		startButton.setMaxWidth(125);
-<<<<<<< HEAD
 		startButton.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
 		//adding start button to layout
 		startLayout.getChildren().addAll( startButton);
 		// setting default scene dimensions
 		Scene startScene = new Scene(startLayout, 1275, 680);
 		window.setScene(startScene);
-		
+
 		//leh try catch?
 		startButton.setOnAction(e -> {
-=======
-		startButton.setAlignment(Pos.CENTER);
-		
-		
-		
-		startButton.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
-		startlayout.getChildren().addAll(gamenamelogoViewVBox ,startButton);
-		// Default scene dimentions 
-		Scene startScene = new Scene(startlayout, 1275, 680);
-		window.setScene(startScene);
-		
-		
-		startButton.setOnMouseClicked(e -> {
-			
->>>>>>> bb2221f4b5b4df7ccfdaf9399b18502efa34c9d9
 			try {
-				
 				scene2(window);
 			} catch (IOException e1) {
-				
+
 				e1.printStackTrace();
 			}
 		});
 
 	}
-	
-	
+
+
 	public void scene2(Stage window) throws IOException {
 		//adding the next button and setting its size
 		Button next = new Button("Next");
@@ -133,7 +92,6 @@ public class HomeScreen extends Application {
 		next.setMaxHeight(50);
 		next.setMaxWidth(140);
 		next.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
-<<<<<<< HEAD
 		//adding a text field
 		TextField playerOne = new TextField();
 		playerOne.setPromptText("Player Name     ");
@@ -159,79 +117,16 @@ public class HomeScreen extends Application {
 		nameLayout.setBackground(new Background(bg));
 		window.setScene(writeName);
 
-=======
-		next.setTranslateY(-40);
-		
-		next.setTranslateY(150);;
-		
-		
-		Image enteryournamelogo = new Image("file:images/enteryournamelogo.png");
-		ImageView enteryournamelogoView = new ImageView(enteryournamelogo);
-		
-		HBox enteryournamelogoViewVbox = new HBox();
-		enteryournamelogoViewVbox.getChildren().add(enteryournamelogoView);
-		enteryournamelogoViewVbox.setAlignment(Pos.TOP_CENTER);
-		
-		
-		
-		
 
-		TextField playerone = new TextField();
-		playerone.setPromptText("Player Name");
-		playerone.setMaxSize(400, 50);
-		playerone.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
-		playerone.setFocusTraversable(true);
-		playerone.setAlignment(Pos.CENTER);
-		
 
-		
-		StackPane namelayout = new StackPane();
-		playerone.setAlignment(Pos.CENTER_RIGHT);
-		
-		Image nameimage = new Image("file:images/gameHomeScreen.jpg");
-		ImageView nameview = new ImageView(nameimage);
-		Group nameg = new Group();
-		nameg.getChildren().addAll(nameview);
-		BackgroundImage bg = new BackgroundImage(new Image("file:images/scene2wallpaper.png"),
-				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-				BackgroundPosition.CENTER, new BackgroundSize(1.0 ,1.0,true,true, false, false) );
 
-		
-		namelayout.getChildren().addAll(playerone,enteryournamelogoViewVbox,next);
-		Scene writename = new Scene(namelayout, 1275, 680);
-		namelayout.setBackground(new Background(bg));
-		window.setScene(writename);
-		//Button tool = new Button("Instructions") ;
-		//tool.setMaxHeight(50);
-		//tool.setMaxWidth(140);
-		//tool.setStyle("-fx-font: 20 arial; -fx-base: #b6e7c9;");
-		
-		
-		//namelayout.getChildren().addAll(cairoButton,romeButton,spartaButton);
-		
->>>>>>> bb2221f4b5b4df7ccfdaf9399b18502efa34c9d9
-		
-		
-		
-		
-		
-		
-		
-<<<<<<< HEAD
+
+
+
+
 		next.setOnAction(e -> {
 			if (playerOne.getText().isEmpty()) {
 
-=======
-		next.setOnMouseClicked(e -> {
-			if (playerone.getText().isEmpty()) {
-			
-				try {
-					scene2(window);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
->>>>>>> bb2221f4b5b4df7ccfdaf9399b18502efa34c9d9
 			} else {
 				try {
 					scene3(window, playerOne.getText());
@@ -240,23 +135,23 @@ public class HomeScreen extends Application {
 					e1.printStackTrace();
 				}
 			}
-			
+
 
 		});
-		
+
 	}
-	
+
 	public void scene3(Stage window, String playerName) throws IOException	{
 
 		Background bg = Createbackground1("worldmap.jpg");
 
 
-		
+
 		Button cairoButton = new Button ();
 		Button spartaButton = new Button ();
 		Button romeButton = new Button ();
-		
-		
+
+
 		Image cairoImage = new Image("file:images/cairologo.jpg");
 		ImageView cairoImageView = new ImageView(cairoImage);
 		cairoImageView.setFitHeight(200);;
@@ -268,7 +163,7 @@ public class HomeScreen extends Application {
 		spartaImageView.setFitHeight(200);;
 		spartaImageView.setFitWidth(200);
 		spartaButton.setGraphic(spartaImageView);
-		
+
 		Image romeImage = new Image("file:images/romelogo.png");
 		ImageView romeImageView = new ImageView(romeImage);
 		romeImageView.setFitHeight(200);;
@@ -277,48 +172,48 @@ public class HomeScreen extends Application {
 		romeButton.setGraphic(romeImageView);
 
 		playerCityName="";
-		if (cairoButton.isPressed()){ 
+		if (cairoButton.isPressed()){
 			this.playerCityName = "Cairo";
 		}
-		
-		if (spartaButton.isPressed()){ 
+
+		if (spartaButton.isPressed()){
 			this.playerCityName = "Sparta";
 		}
-		
-		
-		if (romeButton.isPressed()){ 
+
+
+		if (romeButton.isPressed()){
 			this.playerCityName = "Rome";
-			
+
 		}
-			
+
 		HBox buttonsHBox = new HBox();
 		buttonsHBox.getChildren().add(cairoButton);
 		buttonsHBox.getChildren().add(spartaButton);
 		buttonsHBox.getChildren().add(romeButton);
-		
-		buttonsHBox.setAlignment(Pos.CENTER);	
-		
-		
-		
-		
-		
+
+		buttonsHBox.setAlignment(Pos.CENTER);
+
+
+
+
+
 		Image pickYourCity = new Image("file:images/pickyourcitylogo.png");
 		ImageView pickYourCityView = new ImageView(pickYourCity);
 		VBox chooseYourCityVbox= new VBox();
 		chooseYourCityVbox.getChildren().add(pickYourCityView);
 		chooseYourCityVbox.setAlignment(Pos.BASELINE_CENTER);
-		
-		
-		
+
+
+
 		StackPane stackPane = new StackPane();
 		stackPane.setBackground(bg);
 		stackPane.getChildren().addAll(chooseYourCityVbox ,buttonsHBox);
 		Scene worldmap = new Scene(stackPane, 1275, 680);
 		window.setScene(worldmap);
-		
-		
-		
-		
+
+
+
+
 
 		cairoButton.setOnAction(e -> {
 			playerCityName = "Cairo";
@@ -335,7 +230,7 @@ public class HomeScreen extends Application {
 				e1.printStackTrace();
 			}
 		});
-		
+
 		spartaButton.setOnAction(e -> {
 			playerCityName = "Sparta" ;
 			try {
@@ -352,9 +247,9 @@ public class HomeScreen extends Application {
 			}
 		});
 		romeButton.setOnAction(e -> {
-			
+
 			playerCityName = "Rome";
-					
+
 			try {
 				game = new Game (playerName,"Rome");
 			} catch (IOException e1) {
@@ -368,30 +263,18 @@ public class HomeScreen extends Application {
 				e1.printStackTrace();
 			}
 		});
-		
-		
-		
-		
-		
+
+
+
+
+
 	}
-	
-	
+
+
 	public void worldMapView(Stage window) throws IOException	{
 		//creating label to show food, gold , and other info
 		Label label = new Label("player name: " + game.getPlayer().getName()  + "\n Player City: " + playerCityName + "\n Turn Count: " +
 				game.getCurrentTurnCount() + "\n Food: " + game.getPlayer().getFood() + "\n Gold: "+ game.getPlayer().getTreasury());
-<<<<<<< HEAD
-=======
-		
-		BackgroundImage bg = new BackgroundImage(new Image("file:images/calmwallpaper.jpg"),
-				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-				BackgroundPosition.CENTER, new BackgroundSize(1.0 ,1.0,true,true, false, false) );
-
-		
-		
-		Label label1 = new Label( );
-		label1.setLayoutX(200);
->>>>>>> bb2221f4b5b4df7ccfdaf9399b18502efa34c9d9
 
 		//creating a borderpane to be the main layout
 		BorderPane pageLayout = new BorderPane();
@@ -403,11 +286,11 @@ public class HomeScreen extends Application {
 
 
 		HBox citiesHBox = new HBox(50);
-		
+
 		Button cairoButton = new Button ();
 		Button spartaButton = new Button ();
 		Button romeButton = new Button ();
-		
+
 		//cities hbox
 		Image cairoImage = new Image("file:images/cairotextlogo.png");
 		ImageView cairoImageView = new ImageView(cairoImage);
@@ -420,21 +303,20 @@ public class HomeScreen extends Application {
 		spartaImageView.setFitHeight(200);;
 		spartaImageView.setFitWidth(200);
 		spartaButton.setGraphic(spartaImageView);
-		
+
 		Image romeImage = new Image("file:images/rometextlogo.png");
 		ImageView romeImageView = new ImageView(romeImage);
 		romeImageView.setFitHeight(200);;
 		romeImageView.setFitWidth(200);
 		romeButton.setGraphic(romeImageView);
 		romeButton.setGraphic(romeImageView);
-		
-		
+
+
 		citiesHBox.getChildren().addAll(cairoButton,spartaButton,romeButton);
 		citiesHBox.setAlignment(Pos.CENTER);
 
 		//adding to main layout
 		pageLayout.setCenter(citiesHBox);
-		pageLayout.setBackground(new Background(bg));
 		Scene worldMapView = new Scene(pageLayout , 1275, 680);
 
 		
