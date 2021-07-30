b78ac2d9b6e25bdb19541f0e6800bc5fe7388911
mostafa1912 <mostafa1912@gmail.com>
2021-07-30 03:56:54 +0200

    no changes / just binary file edits

--- EmpireM2/src/GUI/BuildBuilding.java
+++ EmpireM2/src/GUI/BuildBuilding.java
@@ -1,23 +1,15 @@
 package GUI;
 
-import engine.Game;
 import engine.Player;
 import exceptions.NotEnoughGoldException;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Modality;
